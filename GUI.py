389376f9c8372978d13d9f167be911f808367de1
Rafael Basilio <rafaelbasiliochaves@gmail.com>
2019-08-23 21:38:09 +0200

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -22,6 +22,7 @@ class application:
         self.tab3 = ttk.Frame(self.tab_control)
         self.tab4 = ttk.Frame(self.tab_control)
         self.tab5 = ttk.Frame(self.tab_control)
+        self.tab6 = ttk.Frame(self.tab_control)
         
         # Tab for entering inputs and calculating the whole things
         self.tab_control.add(self.tab1, text='Inputs')
@@ -38,6 +39,29 @@ class application:
         
         # Tractive forces
         self.tab_control.add(self.tab5, text='Tractive Forces')
+
+        # About
+        self.tab_control.add(self.tab6, text='About')
+        
+        # You're free to change the entire code, but please do not delete this part. 
+        # It is very important to me to receive feedbacks.
+        Title = "Geabox Calculator v1.0"
+        appInfos ="""
+        This software was originally designed by Rafael Basilio Chaves.
+        The original version can be found at: https://github.com/rafaelbasilio/gearboxCalculator
+        Feel free to distribute and modify it as you want.\n
+        This sofrware is 100% free and OpenSource and it must stay like this. 
+        The commercial use of it is allowed, but it is extrictly vorbiden to sell this software for any price.
+        Feedbacks, improvements and suggestions are always welcome!
+        Please get in touch with me: doutorautomovel@gmail.com
+        Have Fun! 
+        """
+        self.title = Label(self.tab6, text=Title, font=("Helvetica", 14, 'bold'))
+        self.title.pack(pady=30)
+        self.information = Label(self.tab6, text=appInfos)
+        self.information.pack()
+        
+        
         
         ###################################################################
         #######################  Tab 1: Inputs   ##########################
